af44aac3aefbd51fd7b79f906428a776ee55d11b
John Torres <apodpm@gmail.com>
2022-11-21 21:18:00 +0800

    home: mint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-
 import type { NextPage } from "next";
 import Head from "next/head";
-
-// import Hero from "../components/Debug/Hero";
-import Account from "../components/Debug/Account";
-// import Test from "../components/Debug/test";
-import Max7 from "../components/Layout/max7";
 import Mint from "../components/Monarch/mint";
-
 
 const Page: NextPage = () => {
     return (
         <div>
             <Head>
-                <title>RainbowKit DEMO</title>
-                <meta name="description" content="RainbowKit DEMO, with wagmi, react, next" />
+                <title>Claim MonarchMixer</title>
+                <meta name="description" content="Claim MonarchMixer ERC1155 NFT" />
             </Head>
-            <div>
-                <Max7>
-                    <div className="py-20">
-                        <Account />
-                    </div>
-                </Max7>
+            <div className="bg-gray-800">
+                <Mint />
             </div>
         </div>
     );
