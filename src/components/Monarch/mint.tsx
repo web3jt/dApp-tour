431f1bfbf16a14f2ec3c1c41ba5c54021d742891
John Torres <apodpm@gmail.com>
2022-11-21 20:10:53 +0800

    mint: preview image

--- src/components/Monarch/mint.tsx
+++ src/components/Monarch/mint.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'usehooks-ts';
+import Image from 'next/image';
+import Link from 'next/link';
 import { ethers } from 'ethers';
 import {
     useAccount,
@@ -10,8 +10,34 @@ import {
     useWaitForTransaction,
 } from "wagmi";
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
-
 import CONTRACT_ABI from '../../config/abi/monarchMixer';
+
+import TokenImage0 from '../../../public/images/tokens/cloud-illustration-indigo-400.svg';
+import TokenImage1 from '../../../public/images/tokens/cloud-illustration-indigo-400.svg';
+import TokenImage2 from '../../../public/images/tokens/cloud-illustration-indigo-400.svg';
+
+type TokenMeta = {
+    imageSrc: any,
+    imageAlt: string,
+}
+
+const TOKEN_METAS = [
+    {
+        imageSrc: TokenImage0,
+        imageAlt: 'Default Image',
+    },
+    {
+        imageSrc: TokenImage1,
+        imageAlt: 'Token #1 Image',
+    },
+    {
+        imageSrc: TokenImage2,
+        imageAlt: 'Token #2 Image',
+    },
+]
+
+
+
 
 const MONARCH_MIXER_CONTRACT_CONFIG = {
     address: '0x60B2D8fF61EA7adbee55BfC574F68AFFBaA9441b',
@@ -126,15 +152,23 @@ export default function Example() {
     }, [mintCode, mintCodeError, prepareMint]);
 
 
-    // interface TokenMeta {
-    //     imageSrc: string,
-    //     imageAlt: string,
-    // }
+    const [tokenMeta, setTokenMeta] = useState<TokenMeta>(TOKEN_METAS[0]);
+    useEffect(() => {
+        if (
+            mintCodeJSON
+            &&
+            0 < mintCodeJSON.tokenId
+            &&
+            TOKEN_METAS.length >= mintCodeJSON.tokenId
+        ) {
+            setTokenMeta(TOKEN_METAS[mintCodeJSON.tokenId]);
+            return;
+        }
 
-    // const [tokenMeta, setTokenMeta] = useState<TokenMeta>({
-    //     imageSrc: 'https://tailwindui.com/img/ecommerce-images/confirmation-page-04-product-01.jpg',
-    //     imageAlt: 'Off-white t-shirt with circular dot illustration on the front of mountain ridges that fade.',
-    // });
+        setTokenMeta(TOKEN_METAS[0]);
+    }, [mintCodeJSON]);
+
+
 
     enum TipType {
         Info,
@@ -365,80 +399,78 @@ export default function Example() {
 
 
     return (
-        <>
-            <div className="relative overflow-hidden">
-                <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
-                    <div className="mx-auto max-w-7xl lg:px-8">
-                        <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-                            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
-                                <div className="lg:py-24">
-                                    <a
-                                        href="#"
-                                        className="inline-flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
-                                    >
-                                        <span className="rounded-full bg-indigo-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
-                                            Discord
-                                        </span>
-                                        <span className="ml-4 text-sm">
-                                            Welcome to Monarch Family
-                                        </span>
-                                        <ChevronRightIcon className="ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
-                                    </a>
-                                    <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                                        <span className="block">
-                                            Claim Monarch-Mixer
-                                        </span>
-                                        <span className="block text-indigo-400">
-                                            Join our Journey
-                                        </span>
-                                    </h1>
-                                    <p className="mt-3 text-base text-gray-400 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                                        Monarch Mixer are NFT mementos, minted in recognition for our early supporters.
-                                    </p>
-                                    <div className="mt-10 sm:mt-12">
-                                        <div className="sm:mx-auto sm:max-w-xl lg:mx-0">
-                                            <div className="sm:flex">
-                                                <div className="min-w-0 flex-1">
-                                                    <label htmlFor="mint-code" className="sr-only">
-                                                        Mint-Code
-                                                    </label>
-                                                    <input
-                                                        id="mint-code"
-                                                        type="mint-code"
-                                                        onChange={(e) => setMintCode(e.currentTarget.value.trim())}
-                                                        placeholder="Enter your Mint-Code"
-                                                        className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
-                                                    />
-                                                </div>
-                                                <div className="mt-3 sm:mt-0 sm:ml-3">
-                                                    <button
-                                                        type="button"
-                                                        disabled={!writeMint.write || writeMint.isLoading || waitForMintTx?.isLoading}
-                                                        onClick={onClickMint}
-                                                        className="block w-full rounded-md bg-indigo-500 py-3 px-4 font-medium text-white shadow hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
-                                                    >
-                                                        Claim Now
-                                                    </button>
-                                                </div>
+        <div className="relative overflow-hidden">
+            <div className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+                <div className="mx-auto max-w-7xl lg:px-8">
+                    <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                        <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+                            <div className="lg:py-24">
+                                <a
+                                    href="#"
+                                    className="inline-flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+                                >
+                                    <span className="rounded-full bg-indigo-500 px-3 py-0.5 text-sm font-semibold leading-5 text-white">
+                                        Discord
+                                    </span>
+                                    <span className="ml-4 text-sm">
+                                        Welcome to Monarch Family
+                                    </span>
+                                    <ChevronRightIcon className="ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
+                                </a>
+                                <h1 className="mt-4 text-4xl font-bold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                                    <span className="block">
+                                        Claim Monarch-Mixer
+                                    </span>
+                                    <span className="block text-indigo-400">
+                                        Join our Journey
+                                    </span>
+                                </h1>
+                                <p className="mt-3 text-base text-gray-400 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                                    Monarch Mixer are NFT mementos, minted in recognition for our early supporters.
+                                </p>
+                                <div className="mt-10 sm:mt-12">
+                                    <div className="sm:mx-auto sm:max-w-xl lg:mx-0">
+                                        <div className="sm:flex">
+                                            <div className="min-w-0 flex-1">
+                                                <label htmlFor="mint-code" className="sr-only">
+                                                    Mint-Code
+                                                </label>
+                                                <input
+                                                    id="mint-code"
+                                                    type="mint-code"
+                                                    onChange={(e) => setMintCode(e.currentTarget.value.trim())}
+                                                    placeholder="Enter your Mint-Code"
+                                                    className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+                                                />
                                             </div>
-                                            <Tip />
+                                            <div className="mt-3 sm:mt-0 sm:ml-3">
+                                                <button
+                                                    type="button"
+                                                    disabled={!writeMint.write || writeMint.isLoading || waitForMintTx?.isLoading}
+                                                    onClick={onClickMint}
+                                                    className="block w-full rounded-md bg-indigo-500 py-3 px-4 font-medium text-white shadow hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+                                                >
+                                                    Claim Now
+                                                </button>
+                                            </div>
                                         </div>
+                                        <Tip />
                                     </div>
                                 </div>
                             </div>
-                            <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
-                                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                                    <img
-                                        className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                                        src="https://tailwindui.com/img/component-images/cloud-illustration-indigo-400.svg"
-                                        alt=""
-                                    />
-                                </div>
+                        </div>
+                        <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
+                            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                                <Image
+                                    className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                                    src={tokenMeta.imageSrc}
+                                    alt={tokenMeta.imageAlt}
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
