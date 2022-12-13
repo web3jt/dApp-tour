1175ca4ccc6207d52bcdec2ec8a302b5c8362616
John Torres <apodpm@gmail.com>
2022-12-13 15:49:49 +0800

    add: LogoCloud

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Def from "../components/Monarch/Def";
 import Mixer from "../components/Monarch/Mixer";
 import JoinDiscord from "../components/Monarch/JoinDiscord";
 import Team from "../components/Monarch/Team";
+import LogoCloud from "../components/Monarch/LogoCloud";
 // import Team2 from "../components/Monarch/Team2";
 import Container, { OuterContainer, InnerContainer } from "../components/Layout/Container";
 
@@ -22,6 +23,7 @@ const Page: NextPage = () => {
                 <Def />
                 <Mixer />
                 <Team />
+                <LogoCloud />
                 {/* <Team2 /> */}
                 <JoinDiscord />
             </Container>
